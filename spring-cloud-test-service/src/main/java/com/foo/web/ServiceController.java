14573b090ec3808e9174b8cf2c2be459ac84fefd
blackkensai <blackkensai@gmail.com>
2017-04-05 17:36:01 +0800

    config server

--- spring-cloud-test-service/src/main/java/com/foo/web/ServiceController.java
+++ spring-cloud-test-service/src/main/java/com/foo/web/ServiceController.java
@@ -1,19 +1,26 @@
 package com.foo.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.netflix.config.DynamicPropertyFactory;
+import com.netflix.config.DynamicStringProperty;
+
 @RestController
+@RefreshScope
 public class ServiceController {
 	@Autowired
 	private EurekaInstanceConfigBean configBean;
 
+	DynamicStringProperty nameprop = DynamicPropertyFactory.getInstance().getStringProperty("my.name", "unknown one");
+
 	@RequestMapping("/hello")
 	public @ResponseBody String hello(@RequestParam("n") String name) {
-		return String.format("hello, %s from %s", name, configBean.getInstanceId());
+		return String.format("hello, %s from %s at %s", name, nameprop.get(), configBean.getInstanceId());
 	}
 }
